7a883ac0c5f5c98301ea34e7d9b8654f7cacdc87
julicode97 <jlondono433@gmail.com>
2023-08-04 18:44:34 -0500

    Conoce Livewire y aprende a usarlo

---
+++ app/Http/Livewire/LandingPage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LandingPage extends Component
+{
+    public $email;
+    public function subscribe()
+    {
+
+    }
+    public function render()
+    {
+        return view('livewire.landing-page');
+    }
+}
