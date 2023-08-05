47846e1f72c70b6a24b58682968a570d20a7664e
julicode97 <jlondono433@gmail.com>
2023-08-05 18:23:18 -0500

    Mejora las funcionalidades y la experiencia del usuario usando Livewire

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -44,7 +44,7 @@
                 type="email"
                 name="email"
                 placeholder="Email address"
-                wire:model="email"
+                wire:model.defer="email"
             ></x-text-input>
             <span class="text-gray-100 text-xs">
                 {{ $errors->has('email') ? $errors->first('email') : 'We will send you a confirmation email. asfdsa' }}
@@ -52,7 +52,8 @@
             <x-button type="submit"
                 class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center"
             >
-                Get In
+                <span class="animate-spin" wire:loading wire:target="subscribe">&#9696;</span>
+                <span wire:loading.remove wire:target="subscribe">Get In</span>
             </x-button>
         </form>
     </x-modal-tall>
