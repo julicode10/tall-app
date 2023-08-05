31266688b74c1c0d482e573aa05c2b5fef64fcc0
julicode97 <jlondono433@gmail.com>
2023-08-05 15:48:33 -0500

    Creando un suscriptor en cuanto se envía el formulario

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -3,13 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Subscriber;
 
 class LandingPage extends Component
 {
     public $email;
+
     public function subscribe()
     {
-
+        $subscriber = Subscriber::create([
+            'email' => $this->email
+        ]);
+        $this->reset('email');
     }
     public function render()
     {
