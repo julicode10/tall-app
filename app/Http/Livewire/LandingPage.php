b4edbb2589063e9fc6fd7186afd251d3131bc00b
julicode97 <jlondono433@gmail.com>
2023-08-05 16:56:53 -0500

    Confirma la cuenta de correo de un suscriptor usando Laravel

--- app/Http/Livewire/LandingPage.php
+++ app/Http/Livewire/LandingPage.php
@@ -4,6 +4,9 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Subscriber;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class LandingPage extends Component
 {
@@ -15,9 +18,23 @@ class LandingPage extends Component
     public function subscribe()
     {
         $this->validate();
-        $subscriber = Subscriber::create([
-            'email' => $this->email
-        ]);
+        DB::transaction(function () {
+            $subscriber = Subscriber::create([
+                'email' => $this->email
+            ]);
+            $notification = new VerifyEmail;
+            $notification::createUrlUsing(function($notifiable){
+                return URL::temporarySignedRoute(
+                    'subscribers.verify',
+                    now()->addMinutes(30),
+                    [
+                        'subscriber' => $notifiable->getKey()
+                    ]
+                );
+            });
+            $subscriber->notify($notification);
+        }, $deatLockRetries = 5);
+
         $this->reset('email');
     }
     public function render()
