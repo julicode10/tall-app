df7b6b07982e46e7d451e88c3481fe18d522a3ae
julicode97 <jlondono433@gmail.com>
2023-08-05 19:19:13 -0500

    Prepara el sistema de suscriptores para tener un dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Subscriber;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +17,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         Subscriber::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@email.com',
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <p class="text-2xl text-gray-600 font-bold mb-6 underline dark:text-white">
+                        Actions
+                    </p>
+                    <ul class="list-disc pl-5">
+                        <li class="text-blue-500 hover:underline">
+                            <a href="#">
+                                Manage subscribers
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
